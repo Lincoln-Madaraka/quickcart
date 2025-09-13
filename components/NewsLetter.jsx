47062bb90203f067eb4f401b066e3c0c3c0590a8
Lincoln Madaraka <117019629+Lincoln-Madaraka@users.noreply.github.com>
2025-09-13 04:08:09 +0300

    theme

--- components/NewsLetter.jsx
+++ components/NewsLetter.jsx
@@ -3,12 +3,11 @@ import React from "react";
 const NewsLetter = () => {
   return (
     <div className="flex flex-col items-center justify-center text-center space-y-2 pt-8 pb-14">
-      <h1 className="md:text-4xl text-2xl font-medium">
+      <h1 className="md:text-4xl text-2xl font-medium dark:text-yellow-400">
         Subscribe now & get 20% off
       </h1>
-      <p className="md:text-base text-gray-500/80 pb-8">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry.
+      <p className="md:text-base text-gray-500/80 pb-8 dark:text-white">
+        Receive exclusive offers from our site whenever they drop
       </p>
       <div className="flex items-center justify-between max-w-2xl w-full md:h-14 h-12">
         <input
