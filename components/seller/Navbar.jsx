f0513bff19e7fc3264dfb3f9ce07c58789133795
Lincoln Madaraka <117019629+Lincoln-Madaraka@users.noreply.github.com>
2025-09-14 10:58:06 +0300

    seller

--- components/seller/Navbar.jsx
+++ components/seller/Navbar.jsx
@@ -13,33 +13,29 @@ const Navbar = () => {
   const { router } = useAppContext()
 
   return (
-    <div className='flex items-center px-4 md:px-8 py-3 justify-between border-b dark:bg-gray-800'>
-      <Image onClick={()=>router.push('/')} className='w-28 lg:w-32 cursor-pointer dark:hidden block' src={assets.logo} alt="" />
-      <div className="hidden dark:flex items-center">
-              <Image
-                src="/favicon.png"
-                alt="QuickCart Logo"
-                width={34}
-                height={34}
-              />
-              <span
-                className={`${lexend.className} ml-0 text-2xl font-semibold tracking-tight`}
-              >
-                uickCart
-              </span>
-            </div>
-              <Image
-                src="/favicon.png"
-                alt="QuickCart Logo"
-                width={34}
-                height={34}
-              />
-              <span
-                className={`${lexend.className} ml-0 text-2xl font-semibold tracking-tight hidden dark:block`}
-              >
-                uickCart
-              </span>
-          
+ <div className='flex items-center px-4 md:px-8 py-3 justify-between border-b dark:bg-gray-800'>
+  {/* Light mode logo */}
+  <Image
+    onClick={() => router.push('/')}
+    className='w-28 lg:w-32 cursor-pointer block dark:hidden'
+    src={assets.logo}
+    alt="QuickCart Logo Light"
+  />
+
+  {/* Dark mode logo */}
+  <div className="hidden dark:flex items-center">
+    <Image
+      src="/favicon.png"
+      alt="QuickCart Logo Dark"
+      width={34}
+      height={34}
+    />
+    <span
+      className={`${lexend.className} ml-0 text-2xl font-semibold tracking-tight dark:text-white`}
+    >
+      uickCart
+    </span>
+  </div>
       <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
   )
